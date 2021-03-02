fc5f953ce9594c1666037a77931d448bb454ec65
cuboids <44651818+cuboids@users.noreply.github.com>
2021-03-02 01:53:13 +0100

    Update kleisli.py

--- kleisli.py
+++ kleisli.py
@@ -1,8 +1,9 @@
 from typing import Tuple
+from collections.abc import Callable
 
 
 def is_even(n: int) -> Tuple[bool, str]:
-    return not n % 2, "is even "
+    return not n % 2, "is_even "
 
 
 def negate(b: bool) -> Tuple[bool, str]:
@@ -11,29 +12,34 @@ def negate(b: bool) -> Tuple[bool, str]:
 
 class Kleisli:
 
-    @staticmethod
-    def copy1(f):
+    def __init__(self, f: Callable) -> None:
+        """ f is a function """
+        self.f = f
+
+    def copy1(self) -> Callable:
         def copy1_f(*args, **kwargs):
-            result = f(*args, **kwargs)
+            result = self.f(*args, **kwargs)
             return result[0]
         return copy1_f
 
-    @staticmethod
-    def copy2(f):
+    def copy2(self) -> Callable:
         def copy2_f(*args, **kwargs):
-            result = f(*args, **kwargs)
+            result = self.f(*args, **kwargs)
             return result[1]
         return copy2_f
 
-    def compose(self, f, g):
-        def both_fg(*args, **kwargs):
-            b = self.copy1(f)(self.copy1(g)(*args, **kwargs))
-            st_g = self.copy2(g)(*args, **kwargs)
-            st_f = self.copy2(f)(self.copy1(g)(*args, **kwargs))
+    def __add__(self, other):
+        def fg(*args, **kwargs):
+            b = self.copy1()(other.copy1()(*args, **kwargs))
+            st_g = other.copy2()(*args, **kwargs)
+            st_f = self.copy2()(other.copy1()(*args, **kwargs))
             return b, st_g + st_f
-        return both_fg
+        return fg
 
 
-K = Kleisli()
-x = K.compose(negate, is_even)(4)
-print(x)
+example = True
+if example:
+    negate = Kleisli(negate)
+    is_even = Kleisli(is_even)
+    is_odd = negate + is_even
+    print(is_odd(7))
