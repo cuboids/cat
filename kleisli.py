dc419956c38e860a5e30c88e5f16f7523e79fae9
cuboids <44651818+cuboids@users.noreply.github.com>
2021-03-02 12:04:42 +0100

    Update kleisli.py

--- kleisli.py
+++ kleisli.py
@@ -1,5 +1,5 @@
-from typing import Tuple, Any, Optional
 from collections.abc import Callable
+from typing import Tuple, Union, Optional
 
 
 def is_even(n: int) -> Tuple[bool, str]:
@@ -10,6 +10,18 @@ def negate(b: bool) -> Tuple[bool, str]:
     return not b, "Not so! "
 
 
+def safe_root(x: float) -> Union[bool, Tuple[bool, float]]:
+    if x >= 0:
+        return True, x ** .5
+    return False
+
+
+def safe_reciprocal(x: float) -> Union[bool, Tuple[bool, float]]:
+    if x:
+        return True, 1/x
+    return False
+
+
 class Kleisli:
 
     M_APPEND = lambda self, x, y: x + y
@@ -17,8 +29,10 @@ class Kleisli:
 
     def __init__(self, f: Optional[Callable] = None) -> None:
         """ By default it initializes the Kleisli identity function. """
+        self.id = False
         if f is None:
             f = lambda x: (x, self.M_EMPTY)
+            self.id = True
         self.f = f
 
     def copy1(self) -> Callable:
@@ -45,10 +59,45 @@ class Kleisli:
         return Kleisli(fg)
 
 
-example = 1
+example = True
 if example:
     negate = Kleisli(negate)
     is_even = Kleisli(is_even)
     kleisli_id = Kleisli()
     is_odd = kleisli_id + negate + kleisli_id + is_even
     print(is_odd(9))
+
+
+class Partial:
+
+    M_EMPTY = 0
+
+    def __init__(self, f: Optional[Callable] = None) -> None:
+        """ By default it initializes the Kleisli identity function. """
+        self.id = False
+        if f is None:
+            f = lambda x: (True, x)
+            self.id = True
+        self.f = f
+
+    def __call__(self, *args, **kwargs):
+        return self.f(*args, **kwargs)
+
+    def __add__(self, other):
+        def fg(*args, **kwargs):
+            y = other.f(*args, **kwargs)
+            if not y:
+                return False
+            z = self.f(y[1])
+            if not z:
+                return False
+            return z
+        return Partial(fg)
+
+
+example = False
+if example:
+    safe_root = Partial(safe_root)
+    safe_reciprocal = Partial(safe_reciprocal)
+    safe_root2 = safe_root + Partial()
+    print(safe_root2(-9))
