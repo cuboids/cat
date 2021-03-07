218edfd8df6a5e217d02fbaa20ea653c7b3c7ac3
cuboids <44651818+cuboids@users.noreply.github.com>
2021-03-07 21:56:29 +0100

    Update shape.py

--- shape.py
+++ shape.py
@@ -1,35 +1,58 @@
 import abc
 from abc import ABC
 import math
+import random
 
 
 class Shape(ABC):
+    def __init__(self):
+        self.number = random.choice(range(10))
+
     @abc.abstractmethod
     def area(self):
         raise NotImplementedError
 
+    @abc.abstractmethod
+    def circ(self):
+        raise NotImplementedError
+
 
 class Circle(Shape):
-    def __init__(self, r: float = 1) -> None:
+    def __init__(self, radius: float = 1):
         super().__init__()
-        self.r = r
+        self.radius = radius
 
     def area(self):
-        return math.pi * self.r ** 2
+        return math.pi * self.radius ** 2
+
+    def circ(self):
+        return 2 * math.pi * self.radius
 
 
 class Rect(Shape):
-    def __init__(self, d: float = 1, h: float = 1) -> None:
+    def __init__(self, depth: float = 1, height: float = 1):
         super().__init__()
-        self.d = d
-        self.h = h
+        self.depth = depth
+        self.height = height
 
     def area(self):
-        return self.d * self.h
+        return self.depth * self.height
+
+    def circ(self):
+        return 2.0 * (self.depth + self.height)
+
+
+class Square(Rect):
+    def __init__(self, depth: float = 1):
+        super().__init__()
+        self.height = self.depth = depth
 
 
 r = Rect(2, 4)
-c = Circle(3/math.sqrt(math.pi))
+c1 = Circle(3/math.sqrt(math.pi))
+c2 = Circle(3/math.pi)
+s = Square(3)
 assert r.area() == 8
-assert math.isclose(c.area(), 9)
-assert issubclass(Rect, Shape)
+assert math.isclose(c1.area(), 9)
+assert math.isclose(c2.circ(), 6)
+assert s.area() == 9
