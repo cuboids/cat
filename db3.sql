b848d7ec6d65b0362f079023cc4a09722c1c1986
cuboids <44651818+cuboids@users.noreply.github.com>
2021-04-03 16:46:53 +0200

    Update db3.sql

--- db3.sql
+++ db3.sql
@@ -21,27 +21,36 @@ CREATE TABLE Ship(
     PRIMARY KEY (s_id)
 );
 
+
+/* We have added a transport id to the transports relationship.
+   This helps to distinguish multiple transports involving the
+   same ship, captain, and container. Furthermore, we assume
+   that each container has a unique ID (like real containers
+   have). */
+
 DROP TABLE IF EXISTS transports;
 CREATE TABLE transports(
+    t_id INTEGER, -- We include a transport ID
     ca_id INTEGER,
-    co_id INTEGER,
     s_id INTEGER,
-    PRIMARY KEY (ca_id, co_id, s_id),
+    co_id INTEGER,
+    PRIMARY KEY (t_id, ca_id, s_id, co_id),
+    UNIQUE (t_id, co_id), -- Each container is shipped by 1 ship and 1 captain.
     FOREIGN KEY (ca_id) REFERENCES Captain,
     FOREIGN KEY (co_id) REFERENCES Container,
     FOREIGN KEY (s_id) REFERENCES Ship
 );
 
+/* For each container at a particular transport, the
+   destination is recorded. */
+
 DROP TABLE IF EXISTS has_as_destination;
 CREATE TABLE has_as_destination(
-    ca_id INTEGER,
+    t_id INTEGER,
     co_id INTEGER,
-    s_id INTEGER,
     l_id INTEGER,
-    PRIMARY KEY (ca_id, co_id, s_id, l_id),
-    FOREIGN KEY (ca_id) REFERENCES Captain,
-    FOREIGN KEY (co_id) REFERENCES Container,
-    FOREIGN KEY (s_id) REFERENCES Ship,
+    PRIMARY KEY (t_id, co_id),
+    FOREIGN KEY (t_id) REFERENCES transports,
     FOREIGN KEY (l_id) REFERENCES Location
 );
 
